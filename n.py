b2ca292c8fbce62088e47803a851d90cff2d74f2
Solomon Taremwa <adamtblack96@gmail.com>
2019-02-17 13:48:09 +0300

    modidfied the python function

--- n.py
+++ n.py
@@ -4,6 +4,10 @@ def sum(x, y, z):
 def show(x, y, z):
     return("X is {x}, y is {y} and z is {z}")
 
-print(show(15, 6, 7))
+def multiply(a, b, c):
+    return a * b * c
 
+print(sum(15, 6, 7))
+
+print(sum(43, 2, 5))
 
